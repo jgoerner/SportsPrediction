dfd2636c890b726e147f945bac2463d9bf83c992
joshua.goerner@gmail.com <joshua.goerner@gmail.com>
2018-07-31 21:42:52 +0200

    complete example notebooks

--- NBA/src/models.py
+++ NBA/src/models.py
@@ -1,4 +1,5 @@
 from neomodel import (
+    RelationshipTo,
     StructuredNode,
     StringProperty,
 )
@@ -7,8 +8,51 @@ class Team(StructuredNode):
     """Boilerplate for a team node
     
     Attributes:
-        name (str): Name of the team
+        name (str): name of the team
         acr (str): three-letter acronym of the team name
     """
+    
+    #PROPERTIES
     acr = StringProperty(unique_index=True, required=True)
     name = StringProperty(required=True)
+
+
+class Country(StructuredNode):
+    """Boilerplate for a team node
+    
+    Attributes:
+        name (str): name of the country
+    """
+
+    # PROPERTIES
+    name = StringProperty(required=True)
+
+
+class State(StructuredNode):
+    """Boilerplate for a state node
+    
+    Attributes:
+        name (str): name of the state
+        country (Country): country of the state
+    """
+
+    # PROPERTIES
+    name = StringProperty(required=True)
+
+    # RELATIONSHIPS
+    country = RelationshipTo(Country, "IS_IN")
+
+
+class City(StructuredNode):
+    """Boilerplate for a city node
+    
+    Attributes:
+        name (str): name of the city
+        state (State): state of the city
+    """
+    
+    # PROPERTIES
+    name = StringProperty(required=True)
+    
+    # RELATIONSHIPS
+    state = RelationshipTo(State, "IS_IN")
