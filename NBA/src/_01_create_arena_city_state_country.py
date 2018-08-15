d92101a55e873d50f232e7ad5af2a3029dcf9699
jgoerner <joshua.goerner@gmail.com>
2018-08-15 15:43:02 +0200

    Create make graph arena city state country

--- NBA/src/_01_create_arena_city_state_country.py
+++ NBA/src/_01_create_arena_city_state_country.py
@@ -5,12 +5,13 @@ os.chdir("/home/jovyan/work")
 
 import pandas as pd
 
-from models import Arena
+from models import Arena, City, Country, State
 from utils import init_connection, print_information
     
 
 @print_information
 def create_arena_nodes(df):
+    n_nodes = 0
     for _, row in df.drop_duplicates(subset=["stadium"]).iterrows():
         arena = Arena(
             name=row["stadium"],
@@ -19,22 +20,53 @@ def create_arena_nodes(df):
             longitude=row["geo2"]
         )
         arena.save()
+        n_nodes += 1
+    print("created {} nodes".format(n_nodes))
 
 @print_information
-def create_city_nodes():
-    pass  
+def create_city_nodes(df):
+    n_nodes = 0
+    for _, row in df.drop_duplicates(subset=["city"]).iterrows():
+        city = City(name=row["city"])
+        city.save()
+        n_nodes += 1
+    print("created {} nodes".format(n_nodes))
+
 
 @print_information
-def create_country_nodes():
-    pass
+def create_state_nodes(df):
+    n_nodes = 0
+    for _, row in df.drop_duplicates(subset=["state"]).iterrows():
+        state = State(name=row["state"])
+        state.save()
+        n_nodes += 1
+    print("created {} nodes".format(n_nodes))
+
+@print_information
+def create_country_nodes(df):
+    n_nodes = 0
+    for _, row in df.drop_duplicates(subset=["country"]).iterrows():
+        country = Country(name=row["country"])
+        country.save()
+        n_nodes += 1
+    print("created {} nodes".format(n_nodes))
     
 @print_information
-def create_state_nodes():
-    pass
+def connect_arenas_cities_states_countries(df):
+    n_rels = 0
+    for _, row in df.drop_duplicates(subset=["stadium"]).iterrows():
+        # get entities
+        a = Arena.nodes.get(name=row["stadium"])
+        ci = City.nodes.get(name=row["city"])
+        s = State.nodes.get(name=row["state"])
+        co = Country.nodes.get(name=row["country"])
 
-@print_information
-def connect_arenas_cities_states_countries():
-    pass
+        # create relationships
+        a.city.connect(ci)
+        ci.state.connect(s)
+        s.country.connect(co)
+        n_rels += 3
+    print("created {} relationships".format(n_rels))
 
 if __name__ == "__main__":
     # read underlying data
@@ -46,7 +78,7 @@ if __name__ == "__main__":
 
     # create nodes
     create_arena_nodes(df)
-    create_city_nodes()
-    create_country_nodes()
-    create_state_nodes()
-    connect_arenas_cities_states_countries()
+    create_city_nodes(df)
+    create_state_nodes(df)
+    create_country_nodes(df)
+    connect_arenas_cities_states_countries(df)
