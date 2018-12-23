12a03350136ddaf36433a34d6850be10749a0a9d
jgoerner <joshua.goerner@gmail.com>
2018-12-23 06:34:08 +0100

    Include all averaged stats as features

--- NBA/src/features.py
+++ NBA/src/features.py
@@ -238,6 +238,76 @@ FEATURES = {
         sea.name as season,
         s.name as preceeding_season
     ORDER BY sea.name, t.abbreviation
+    """,
+    
+    "avg_starter_complete_stats_preceeding_season":
+    """
+    MATCH 
+        (st:Stat)<-[:HAS_STAT]-(p:Player)-[:STARTS_IN]->(g:Game)-[:TOOK_PLACE_IN]->(sea:Season),
+        (t)-[:SCORED]->(sc:Score)-[:IN_GAME]->(g),
+        (s:Season)
+    WHERE
+        LEFT(g.game_type,7) = LEFT(st.season_type,7)
+    AND
+        (st)-[:IN_SEASON]->(s)
+    AND
+        (st)-[:IN_TEAM]->(t)
+    AND
+        s.name = toString(toInt(SUBSTRING(sea.name, 0, 4))-1) + "/" + toString(toInt(SUBSTRING(sea.name, 5))-1)
+    RETURN 
+        AVG(st.GamesPlayed) as avg_GamesPlayed,
+        AVG(st.Fg2PtAtt) as avg_Fg2PtAtt,
+        AVG(st.Fg2PtAttPerGame) as avg_Fg2PtAttPerGame,
+        AVG(st.Fg2PtMade) as avg_Fg2PtMade,
+        AVG(st.Fg2PtMadePerGame) as avg_Fg2PtMadePerGame,
+        AVG(st.Fg2PtPct) as avg_Fg2PtPct,
+        AVG(st.Fg3PtAtt) as avg_Fg3PtAtt,
+        AVG(st.Fg3PtAttPerGame) as avg_Fg3PtAttPerGame,
+        AVG(st.Fg3PtMade) as avg_Fg3PtMade,
+        AVG(st.Fg3PtMadePerGame) as avg_Fg3PtMadePerGame,
+        AVG(st.Fg3PtPct) as avg_Fg3PtPct,
+        AVG(st.FgAtt) as avg_FgAtt,
+        AVG(st.FgAttPerGame) as avg_FgAttPerGame,
+        AVG(st.FgMade) as avg_FgMade,
+        AVG(st.FgMadePerGame) as avg_FgMadePerGame,
+        AVG(st.FgPct) as avg_FgPct,
+        AVG(st.FtAtt) as avg_FtAtt,
+        AVG(st.FtAttPerGame) as avg_FtAttPerGame,
+        AVG(st.FtMade) as avg_FtMade,
+        AVG(st.FtMadePerGame) as avg_FtMadePerGame,
+        AVG(st.FtPct) as avg_FtPct,
+        AVG(st.OffReb) as avg_OffReb,
+        AVG(st.OffRebPerGame) as avg_OffRebPerGame,
+        AVG(st.DefReb) as avg_DefReb,
+        AVG(st.DefRebPerGame) as avg_DefRebPerGame,
+        AVG(st.Reb) as avg_Reb,
+        AVG(st.RebPerGame) as avg_RebPerGame,
+        AVG(st.Ast) as avg_Ast,
+        AVG(st.AstPerGame) as avg_AstPerGame,
+        AVG(st.Pts) as avg_Pts,
+        AVG(st.PtsPerGame) as avg_PtsPerGame,
+        AVG(st.Tov) as avg_Tov,
+        AVG(st.TovPerGame) as avg_TovPerGame,
+        AVG(st.Stl) as avg_Stl,
+        AVG(st.StlPerGame) as avg_StlPerGame,
+        AVG(st.Blk) as avg_Blk,
+        AVG(st.BlkPerGame) as avg_BlkPerGame,
+        AVG(st.BlkAgainst) as avg_BlkAgainst,
+        AVG(st.BlkAgainstPerGame) as avg_BlkAgainstPerGame,
+        AVG(st.FoulPers) as avg_FoulPers,
+        AVG(st.FoulPersPerGame) as avg_FoulPersPerGame,
+        AVG(st.PlusMinus) as avg_PlusMinus,
+        AVG(st.PlusMinusPerGame) as avg_PlusMinusPerGame,
+        AVG(st.MinSeconds) as avg_MinSeconds,
+        AVG(st.MinSecondsPerGame) as avg_MinSecondsPerGame,
+        t.name as team,
+        t.abbreviation as team_abbreviation,
+        g.game_name as game, 
+        g.game_id as game_id,
+        g.game_type as game_type,
+        sea.name as season,
+        s.name as preceeding_season
+    ORDER BY sea.name, t.abbreviation
     """
 }
 
